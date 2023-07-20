2fadf7053ba8b02345b05e86095d894d13d29759
caronkey <guangzep@gmail.com>
2023-07-20 11:06:30 +0800

    fix sv less than 50

--- parsebamread.cpp
+++ parsebamread.cpp
@@ -312,7 +312,7 @@ int extract_variants_read(struct alignedread* read, HASHTABLE* ht, CHROMVARS* ch
     } else if (region_tag == 2) prev_dup_pos = *prev_bnd_pos;
     ss = firstvar;
     for (i = 0; i < read->cigs; i++) {          //iter through base with CIGAR
-        if(varlist[ss].bnd == 1 && (strlen(varlist[ss].allele1) > 50 || strlen(varlist[ss].allele2) > 50 || strstr(varlist[ss].allele2, "<") || strstr(varlist[ss].allele2, "]") || strstr(varlist[ss].allele2, "]")) ) {
+        if((strlen(varlist[ss].allele1) > 50 || strlen(varlist[ss].allele2) > 50 || strstr(varlist[ss].allele2, "<") || strstr(varlist[ss].allele2, "]") || strstr(varlist[ss].allele2, "]")) ) {
             ss++;
             continue;
         }
